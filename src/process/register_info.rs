adf345266c3a8fada76fd366688a9463920d1f4e
Jason Brown <jasedbrown@gmail.com>
2025-11-27 10:35:49 -0800

    WIP: starting on RegisterInfo macros
    have most of the 64-bit registers in place.
    need 32, 16, and 8high/low registers
    
    also need to figure out teh RegisterValue stuffs ...

--- src/process/register_info.rs
+++ src/process/register_info.rs
@@ -1,27 +1,26 @@
+#![allow(dead_code)]
 //! Module to generate a mapping of supported debug registers
 //! to information about each field in the respective c structs.
 //!
-//! Aprt of what makes this module weird is the intersection of
+//! Part of what makes this module weird is the intersection of
 //! c structs, `libc` crate, `memoffset` crate, the advice from
 //! "Building a Debugger", and the rust macro system.
 // use anyhow::Result;
 // use libc::{user, user_regs_struct, user_fpregs_struct};
-// use nix::sys::ptrace::{getregset, read_user, regset};
 
 use strum::EnumDiscriminants;
 
-#[derive(Clone, Copy, Debug, EnumDiscriminants)]
-#[allow(dead_code)]
+#[derive(Clone, Copy, Debug, EnumDiscriminants, Hash)]
+#[strum_discriminants(name(RegisterValueType))]
 pub enum RegisterValue {
     // placeholder variants ....
     Uint(u32),
-    DoubleFloat(f32),
-    LongDouble(f64),
+    // DoubleFloat(f32),
+    // LongDouble(f64),
 }
 
-#[derive(Clone, Copy, Debug)]
-#[allow(dead_code)]
-enum RegisterType {
+#[derive(Clone, Copy, Debug, Hash)]
+pub enum RegisterType {
     /// 64-bit instructions
     GeneralPurpose,
     SubGeneralPupose,
@@ -34,10 +33,38 @@ enum RegisterType {
 macro_rules! REGISTER_LIST {
     ($macro:ident) => {
         $macro! {
-            // (EnumVariant, struct_field, dwarf_regno)
-            (R15, r15, 15);
-            (R14, r14, 14);
-            (R13, r13, 13);
+            // (EnumVariant, struct_field, dwarf_regno, reg_type, bit-width)
+            // 64-bit registers
+            (RAX, rax, 0, GeneralPurpose, 64);
+            (RDX, rdx, 1, GeneralPurpose, 64);
+            (RCX, rcx, 2, GeneralPurpose, 64);
+            (RSI, rsi, 4, GeneralPurpose, 64);
+            (RDI, rdi, 5, GeneralPurpose, 64);
+            (RBP, rbp, 6, GeneralPurpose, 64);
+            (RSP, rsp, 7, GeneralPurpose, 64);
+            (R8, r8, 8, GeneralPurpose, 64);
+            (R9, r9, 9, GeneralPurpose, 64);
+            (R10, r10, 10, GeneralPurpose, 64);
+            (R11, r11, 11, GeneralPurpose, 64);
+            (R12, r12, 12, GeneralPurpose, 64);
+            (R13, r13, 13, GeneralPurpose, 64);
+            (R14, r14, 14, GeneralPurpose, 64);
+            (R15, r15, 15, GeneralPurpose, 64);
+            (RIP, rip, 16, GeneralPurpose, 64);
+            (EFLAGS, eflags, 49, GeneralPurpose, 64);
+            (CS, cs, 51, GeneralPurpose, 64);
+            (FS, fs, 54, GeneralPurpose, 64);            
+            (GS, gs, 55, GeneralPurpose, 64);
+            (SS, ss, 52, GeneralPurpose, 64);            
+            (DS, ds, 53, GeneralPurpose, 64);
+            (ES, es, 50, GeneralPurpose, 64);
+
+            // ptrace exposes this as the way to get the ID of a syscall.
+            // it has no dwarf id.
+            (ORIG_RAX, orig_rax, -1, GeneralPurpose, 64);
+
+            // 32-bit subregisters. no dwarf IDs
+            (EAX, eax, -1, SubGeneralPupose, 32);
         }
     };
 }
@@ -48,8 +75,9 @@ macro_rules! REGISTER_LIST {
 /// currently only supporting x86_64 and what i find in /usr/include/sys/user.h
 /// as i think that's all that's exposed in `nix::ptrace`
 macro_rules! DEFINE_ENUM {
-    ( $( ($register:ident, $field:ident, $dwarf:expr); )* ) => {
+    ( $( ($register:ident, $field:ident, $dwarf:expr, $reg_type:ident, $size:expr); )* ) => {
         #[derive(Copy, Clone, Debug, Hash, Eq, PartialEq)]
+        #[allow(clippy::upper_case_acronyms)]
         pub enum Register {
             $( $register, )*
         }
@@ -60,24 +88,25 @@ macro_rules! DEFINE_ENUM {
 //     register: Register,
 //     name: String,
 //     dwarf_id: i32,
-//     size: usize, // ???
-//     /// Offset into the `user` struct
-//     offset: usize,
 //     register_type: RegisterType,
-//     format: RegisterValueDiscriminants,
+//     size: usize,                          // ??? (64 bit, 32, 16, 8)
+//     /// Offset into the `user` struct
+//     offset: usize,                        // fix me
+//     format: RegisterValueDiscriminants,  /// ????
 // }
 
 macro_rules! DEFINE_INFO {
-    ( $( ($register:ident, $field:ident, $dwarf:expr); )* ) => {
+    ( $( ($register:ident, $field:ident, $dwarf:expr, $reg_type:ident, $size:expr); )* ) => {
         #[derive(Clone, Debug, Hash)]
         pub struct RegisterInfo {
             pub register: Register,
             pub name: &'static str,
             pub dwarf: u16,
             pub offset: usize,
+            pub size: usize,
+            pub register_type: RegisterType,
         }
 
-        #[allow(dead_code)]
         pub const REGISTERS_INFO: &[RegisterInfo] = &[
             $(
                 RegisterInfo {
@@ -85,6 +114,8 @@ macro_rules! DEFINE_INFO {
                     name: stringify!($field),
                     dwarf: $dwarf,
                     offset: memoffset::offset_of!(libc::user_regs_struct, $field),
+                    size: $size / 8,
+                    register_type: RegisterType::$reg_type,
                 },
             )*
         ];
--- src/process/registers.rs
+++ src/process/registers.rs
@@ -1,14 +1,14 @@
+#![allow(dead_code)]
 use anyhow::Result;
 use libc::{user, user_fpregs_struct, user_regs_struct};
 use memoffset::offset_of;
 use nix::sys::ptrace::{getregset, read_user, regset};
 use nix::unistd::Pid;
-use strum::{AsRefStr, EnumDiscriminants};
 
 use std::collections::HashMap;
 use std::sync::LazyLock;
 
-use crate::process::register_info::{REGISTERS_INFO, Register, RegisterInfo};
+use crate::process::register_info::{REGISTERS_INFO, Register, RegisterInfo, RegisterValue};
 
 // ugh, phf will not work as the keys must be of a limited set of types.
 // Just use lazy/once_cell to bulid a fucking map
@@ -22,46 +22,69 @@ static REGISTERS_MAP: LazyLock<HashMap<Register, RegisterInfo>> = LazyLock::new(
     regs
 });
 
-// rename me
+/// Current state of the registers for the debugged process.
+#[derive(Clone, Debug)]
 pub struct RegisterSnapshot {
-    // layout ideas
-    // 1. field per register
-    // 2. wrapper around the backing c structs
+    pid: Pid,
+
+    // Note: the current implementation is simply wrapping the libc structs.
+    // this is probably sufficient, but i could create a rust-equivalent that pairs
+    // with the RegisterInfo a bit more. we shall see if this impl becomes a burden ...
     user_gp: user_regs_struct,
     user_fp: user_fpregs_struct,
     debug_regs: [u64; 8],
-    // 3.
 }
 
 impl RegisterSnapshot {
-    // pub fn new(_gp_regs: user_regs_struct, _fp_regs: user_fpregs_struct, _debug_regs: [u64; 8]) -> Self {
-    //     Self {}
-    // }
+    fn new(
+        pid: Pid,
+        gp_regs: user_regs_struct,
+        fp_regs: user_fpregs_struct,
+        debug_regs: [u64; 8],
+    ) -> Self {
+        Self {
+            pid,
+            user_gp: gp_regs,
+            user_fp: fp_regs,
+            debug_regs,
+        }
+    }
 
-    // pub fn get_value(&self, register: Register) -> RegisterValue {
+    pub fn get_value(&self, register: Register) -> RegisterValue {
+        // TODO: actually implement me
+        match register {
+            Register::R15 => RegisterValue::Uint(15),
+            _ => RegisterValue::Uint(0),
+        }
+    }
 
-    // }
+    // This is called infrequently enough that just re-reading all the registers
+    // and returning a new instance is not a problem.
+    pub fn set_value(
+        &mut self,
+        _register: Register,
+        _value: RegisterValue,
+    ) -> Result<RegisterSnapshot> {
+        // update register on CPU - ptrace::write_user
 
-    // pub fn set_value(&mut self, register: Register, value: RegisterValue) -> Result<()> {
-    //     // update register on CPU - ptrace::write_user
-
-    //     // update self - or just re-read_all_registers()
-    // }
+        // update self - or just re-read_all_registers()
+        read_all_registers(self.pid)
+    }
 }
 
-// pub fn read_all_registers(pid: Pid) -> Result<RegisterSnapshot> {
-//     let gp_reg = getregset::<regset::NT_PRSTATUS>(pid).unwrap();
-//     let fp_reg = getregset::<regset::NT_PRFPREG>(pid).unwrap();
+pub fn read_all_registers(pid: Pid) -> Result<RegisterSnapshot> {
+    let gp_reg = getregset::<regset::NT_PRSTATUS>(pid).unwrap();
+    let fp_reg = getregset::<regset::NT_PRFPREG>(pid).unwrap();
 
-//     let mut debug_regs = [0; 8];
-//     // read out the debug registers
-//     for i in 0..7 {
-//         let base_regs_offset = offset_of!(user, u_debugreg);
-//         // TODO: don't hardcode the offset
-//         let offset = base_regs_offset + (i * 8);
-//         let reg = read_user(pid, offset as _).unwrap();
-//         debug_regs[i] = reg as u64;
-//     }
+    let mut debug_regs = [0; 8];
+    // read out the debug registers
+    for i in 0..debug_regs.len() {
+        let base_regs_offset = offset_of!(user, u_debugreg);
+        // TODO: don't hardcode the offset
+        let offset = base_regs_offset + (i * 8);
+        let reg = read_user(pid, offset as _).unwrap();
+        debug_regs[i] = reg as u64;
+    }
 
-//     Ok(RegisterSnapshot::new(gp_reg, fp_reg, debug_regs))
-// }
+    Ok(RegisterSnapshot::new(pid, gp_reg, fp_reg, debug_regs))
+}
